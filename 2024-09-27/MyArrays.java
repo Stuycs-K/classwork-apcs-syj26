76b017091ec4be925fc7fda3cba2ae55a643e7a0
Sophia Jin <sjin60@stuy.edu>
2024-09-27 11:35:32 -0400

    wrote testing

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -8,4 +8,22 @@ public class MyArrays {
     ans+="]";
     return ans;
   }
+
+  public static boolean sameValues(int[] ary1, int[] ary2) {
+    for (int i=0; i<ary1.length; i++) {
+      if (ary1[i]!=ary2[i]) {return false;}
+    }
+    return true;
+  }
+
+  public static void testCopy(int[] ary1, int[] ary2) {
+    System.out.println(arrayToString(ary1));
+    System.out.println(arrayToString(ary2));
+    System.out.println("Same values: "+(sameValues(ary1, ary2)));
+    System.out.println("Same address: "+(ary1==ary2));
+  }
+
+  public static void main(String[] args) {
+
+  }
 }
