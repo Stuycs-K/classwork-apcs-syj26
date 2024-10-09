22e5e189bdd7cc34a059396b677faad193113917
Sophia Jin <sjin60@stuy.edu>
2024-10-09 11:13:11 -0400

    added fields and constructors

---
+++ 2024-10-09/Kitty.java
@@ -0,0 +1,12 @@
+public class Kitty {
+  private String name;
+  private int age;
+  public Kitty(String startName, int startAge) {
+    name=startName;
+    age=startAge;
+  }
+  public Kitty() {
+    name="Mittens";
+    age=2;
+  }
+}
