e43fcca4e13f8ae057867fa2ea39f547b3c7683b
Sophia Jin <sjin60@stuy.edu>
2024-10-15 11:38:07 -0400

    wrote static distance in Demo

--- 2024-10-15-point/Demo.java
+++ 2024-10-15-point/Demo.java
@@ -5,7 +5,7 @@ public class Demo{
   to the others.
   */
   public static double distance(Point a, Point b){
-    return 0.0;
+    return Math.sqrt(Math.pow(a.getX()-b.getX(), 2.0) + Math.pow(a.getY()-b.getY(), 2.0));
   }
 
   public static void main(String[]args){
@@ -22,6 +22,8 @@ public class Demo{
     Point p3 = new Point(3,4);
     System.out.println( p3);
     System.out.println( distance(p1,p2));
+    System.out.println( distance(p1,p3));
+    System.out.println( distance(new Point(0,0), p3));
     System.out.println( Point.distance(p1,p2));
     System.out.println( p1.distanceTo(p2));
   }
