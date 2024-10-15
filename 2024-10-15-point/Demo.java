80814a6aec0a12bd9ca265ffec283ea08b2d9d9d
Sophia Jin <sjin60@stuy.edu>
2024-10-15 11:31:17 -0400

    wrote other POint constructor

--- 2024-10-15-point/Demo.java
+++ 2024-10-15-point/Demo.java
@@ -10,7 +10,15 @@ public class Demo{
 
   public static void main(String[]args){
     Point p1 = new Point(1,1);
+    System.out.println(p1);
+    System.out.println(p1==new Point(1, 1));
     Point p2 = new Point(-1,-1);
+    System.out.println(p2);
+    System.out.println(p2==new Point(-1, -1));
+    p1=new Point(p2);
+    System.out.println(p1);
+    System.out.println(p2);
+    System.out.println(p1==p2);
     Point p3 = new Point(3,4);
     System.out.println( p3);
     System.out.println( distance(p1,p2));
--- 2024-10-15-point/Point.java
+++ 2024-10-15-point/Point.java
@@ -7,6 +7,8 @@ public class Point{
 
   //Initialize this Point to have the same values as the other Point.
   public Point(Point other){
+    x=other.getX();
+    y=other.getY();
     //COMPLETE THIS SECOND
     //Then write test cases.
   }
