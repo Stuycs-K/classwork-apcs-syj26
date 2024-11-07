27bfd185f7bb00b010ec107162e6a2c0c885574b
Sophia Jin <sjin60@stuy.edu>
2024-11-07 11:20:20 -0500

    wrote constructor and speak()

---
+++ 2024-11-07-Pets/Animal.java
@@ -0,0 +1,36 @@
+public class Animal {
+  private String noise;
+  private int age;
+  private String name;
+
+  public Animal(String noise, int age, String name){
+    this.noise=noise;
+    this.age=age;
+    this.name=name;
+  }
+
+  //2.
+  /*speak() should print the following message replacing
+  the ?'s with name/age/noise:
+  My name is ?.
+  I am ? years old.
+  I say ?.
+
+  Note: you MUST use getName() getNoise() here! You will see why later.
+  */
+  public void speak(){
+    System.out.println("My name is "+getName()+".");
+    System.out.println("I am "+getAge()+" years old.");
+    System.out.println("I say "+getNoise()+".");
+  }
+
+  public String getName(){
+    return name;
+  }
+  public int getAge() {
+    return age;
+  }
+  public String getNoise(){
+    return noise;
+  }
+}
