db60eb6c4e8abaf7604d3b77e523a41e78c5906d
Sophia Jin <sjin60@stuy.edu>
2024-11-14 11:32:01 -0500

    wrote replaceEmpty, tested

--- 2024-11-14-ArrayList/ArrayListPractice.java
+++ 2024-11-14-ArrayList/ArrayListPractice.java
@@ -17,7 +17,27 @@ public class ArrayListPractice {
     return arr;
   }
 
+  public static void testReplaceEmpty(ArrayList<String> arr) {
+    System.out.println("Before: "+arr);
+    replaceEmpty(arr);
+    System.out.println("After: "+arr);
+  }
+
+  public static void replaceEmpty( ArrayList<String> original){
+  //Modify the ArrayList such that it has all of the empty strings are
+  //replaced with the word "Empty".
+    for (int i = 0; i < original.size(); i++){
+      if (original.get(i).equals("")){
+        original.set(i, "Empty");
+      }
+    }
+  }
+
   public static void main(String[] args) {
     System.out.println(createRandomArray(5));
+    for (int i = 0; i < 5; i++){
+      testReplaceEmpty(createRandomArray(15));
+    }
   }
+
 }
