6548a0f740277bd8f84e5b279c3cfa6e333c96c8
Sophia Jin <sjin60@stuy.edu>
2024-11-18 17:41:34 -0500

    oops i mixed up checksum and sectorId before, anyway wrote a functions that extracts array of frequences of letters

--- 2024-11-18-advent2016/Advent2016D4P1.java
+++ 2024-11-18-advent2016/Advent2016D4P1.java
@@ -2,7 +2,8 @@ import java.util.*;
 import java.io.*;
 
 public class Advent2016D4P1 {
-    public static int checksum(String line) {
+    //return sectorID
+    public static int sectorID(String line) {
         int ans=0;
         int left=0; int right=0; int i=line.length()-1; boolean done=false;
         while (i>=0 && !done) {
@@ -22,6 +23,34 @@ public class Advent2016D4P1 {
         }
         return Integer.parseInt(line.substring(left, right));
     }
+
+    //cuts away the sectorID and checksum
+    public static String cut(String line) {
+        int end=0;
+        int i=0;
+        while (end==0) {
+            if (line.charAt(i)<='9' && line.charAt(i)>='0') {
+                end=i;
+            }
+            i++;
+        }
+        return line.substring(0, end-1);
+    }
+    
+    public static int[][] frequencies(String line) {
+        int[][] ans = new int[26][2];
+        for (int i=0; i<26; i++) {
+            ans[i][0]=i; //represents the letter
+        }
+        line=cut(line);
+        for (int i=0; i<line.length(); i++) {
+            if (line.charAt(i)<='z' && line.charAt(i)>='a') {
+                ans[line.charAt(i)-'a'][1]+=1;
+            }
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         try {
             File file = new File("2016d4.txt");
@@ -29,7 +58,9 @@ public class Advent2016D4P1 {
             String line;
             while (sc.hasNextLine()) {
                 line=sc.nextLine();
-                System.out.println(checksum(line));
+                System.out.println(cut(line));
+                System.out.println(sectorID(line));
+                System.out.println(Arrays.deepToString(frequencies(line)));
             }
             sc.close();
         } catch (FileNotFoundException ex) {
