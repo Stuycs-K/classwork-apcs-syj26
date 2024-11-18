9f157dae2ce8a02356a856383b4683d048f0ecd4
Sophia Jin <sjin60@stuy.edu>
2024-11-18 11:46:54 -0500

    wrote code to change x and y

--- 2024-11-18-advent2016/Advent2016D1P1.java
+++ 2024-11-18-advent2016/Advent2016D1P1.java
@@ -8,19 +8,40 @@ public class Advent2016D1P1 {
     //so 0 is right, 1 is up, 2 is left, 3 is down
     int dx=0;
     int dy=0;
+    int change=0;
     String step = "";
-    Scanner sc = ReadFile.read("2016d1p1.txt");
+    try {
+      File file = new File("2016d1p1.txt");
+      sc = new Scanner(file);
+      sc.close();
+    } catch (FileNotFoundException ex) {
+      System.out.println("File not found");
+    }
     while (sc.hasNext()) {
       step = sc.next();
       step = step.substring(0, step.length()-1); //get rid of comma
-      if step.substring(0, 1).equals("R") {
+      if (step.substring(0, 1).equals("R")) {
         orientation--;
       }
-      if step.substring(0, 1).equals("R") {
+      if (step.substring(0, 1).equals("R")) {
         orientation++;
       }
       orientation%=4;
+      change = Integer.parseInt(step.substring(1));
+      if (orientation==0) {
+        dx+=change;
+      }
+      if (orientation==1) {
+        dy+=change;
+      }
+      if (orientation==2) {
+        dx-=change;
+      }
+      if (orientation==3) {
+        dy-=change;
+      }
     }
+    System.out.println(dx+" "+dy);
   }
 
 }
