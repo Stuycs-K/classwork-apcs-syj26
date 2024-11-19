27c6179ac733d29696e19539cdc8937555a7fa00
Sophia Jin <sjin60@stuy.edu>
2024-11-19 11:36:41 -0500

    wrote and tested isABBA function

--- 2024-11-18-advent2016/Day7.java
+++ 2024-11-18-advent2016/Day7.java
@@ -2,13 +2,19 @@ import java.util.*;
 import java.io.*;
 
 public class Day7 {
+  public static boolean isABBA(String s) {
+    return (s.length()==4 && s.charAt(0)==s.charAt(3) && s.charAt(1)==s.charAt(2) && s.charAt(0)!=s.charAt(1));
+  }
   public static void main(String[] args) {
+    System.out.println(isABBA("aaaa"));
+    System.out.println(isABBA("xoox"));
+    System.out.println(isABBA("apcs"));
     try {
       Scanner sc = new Scanner(new File("2016d7.txt"));
       String line;
       while (sc.hasNextLine()) {
         line=sc.nextLine();
-        System.out.println(line);
+        //System.out.println(line);
       }
     } catch (Exception e) {
       System.out.println("File not found");
