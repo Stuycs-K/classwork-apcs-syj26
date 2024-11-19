a9b964b201697667b05feb369fbf7840f2fda943
Sophia Jin <sjin60@stuy.edu>
2024-11-19 11:39:57 -0500

    wrote hasABBA, tested

--- 2024-11-18-advent2016/Day7.java
+++ 2024-11-18-advent2016/Day7.java
@@ -5,10 +5,23 @@ public class Day7 {
   public static boolean isABBA(String s) {
     return (s.length()==4 && s.charAt(0)==s.charAt(3) && s.charAt(1)==s.charAt(2) && s.charAt(0)!=s.charAt(1));
   }
+  public static boolean hasABBA(String s) {
+    for (int i=0; i<s.length()-3; i++) {
+      if (isABBA(s.substring(i, i+4))) {
+        return true;
+      }
+    }
+    return false;
+  }
   public static void main(String[] args) {
     System.out.println(isABBA("aaaa"));
     System.out.println(isABBA("xoox"));
     System.out.println(isABBA("apcs"));
+    System.out.println(hasABBA("aaaa"));
+    System.out.println(hasABBA("xoox"));
+    System.out.println(hasABBA("apcs"));
+    System.out.println(hasABBA("bexoox"));
+    System.out.println(hasABBA("yexooxd"));
     try {
       Scanner sc = new Scanner(new File("2016d7.txt"));
       String line;
