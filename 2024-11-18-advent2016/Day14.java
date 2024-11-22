fcba2a9c99c1728037f0a273565873f924f6ddcb
Sophia Jin <sjin60@stuy.edu>
2024-11-22 12:35:36 -0500

    fixed reading issue, but I'm still getting the wrong answer?

--- 2024-11-18-advent2016/Day14.java
+++ 2024-11-18-advent2016/Day14.java
@@ -1,41 +1,65 @@
 import java.util.HashSet;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Day14 {
-    public static void main(String[] args) {
-        ArrayList<HashSet<String>> FiveInARow = new ArrayList<HashSet<String>>();
-        for (int index=0; index<10000; index++) {
-            String hash = MD5.hash("qzyelonm", index);
-            for (int i=0; i<hash.length()-4; i++) {
-                FiveInARow.add(new HashSet<String>());
-                if (hash.charAt(i)==hash.charAt(i+1) && 
-                hash.charAt(i)==hash.charAt(i+2) && 
-                hash.charAt(i)==hash.charAt(i+3) &&
-                hash.charAt(i)==hash.charAt(i+4)) {
-                    FiveInARow.get(index).add(hash.substring(i, i+1));
-                }
+    public static void FiveInARowSet(int index, String hash, ArrayList<HashSet<String>> FiveInARow) {
+        for (int i=0; i<hash.length()-4; i++) {
+            if (hash.charAt(i)==hash.charAt(i+1) && 
+            hash.charAt(i)==hash.charAt(i+2) && 
+            hash.charAt(i)==hash.charAt(i+3) &&
+            hash.charAt(i)==hash.charAt(i+4)) {
+                FiveInARow.get(index).add(hash.substring(i, i+1));
             }
         }
-        for (int i=0; i<10000; i++) {
+    }
+    public static void main(String[] args) {
+        String salt="abc";
+        ArrayList<HashSet<String>> FiveInARow = new ArrayList<HashSet<String>>();
+        //set up where a character appears 5 times in a row in the first 1000 hashes
+        for (int index=0; index<1000; index++) {
+            FiveInARow.add(new HashSet<String>());
+            FiveInARowSet(index, MD5.hash(salt, index), FiveInARow);
+        }
+        int index=0;
+        HashSet<Integer> workingIndices = new HashSet<Integer>();
+        while (workingIndices.size()<64) {
+            //increases the SIZE of FiveInARow by 1
+            FiveInARow.add(new HashSet<String>());
+            //sets index (index+1000) of FiveInARow to be the desired HashSet
+            FiveInARowSet(index+1000, MD5.hash(salt, index+1000), FiveInARow);
+            //looking for ThreeInARow of the current hash of the given index
+            String hash = MD5.hash(salt, index);
+            String ThreeInARow = null;
+            boolean triple=false;
+            //find where a character appears 3 in a row
+            for (int i=0; i<hash.length()-2; i++) {
+                if (hash.charAt(i)==hash.charAt(i+1) && hash.charAt(i)==hash.charAt(i+2) && !triple) {
+                    ThreeInARow=hash.substring(i, i+1);
+                }
+            }
+            //adds index to working indices if an element of ThreeInARow is in FiveInARow from index (index+1) to (index+1000)
+            if (ThreeInARow != null) {
+                for (int j=index+1; j<=index+1000; j++) {
+                    if (FiveInARow.get(j).contains(ThreeInARow)) {
+                        System.out.println(ThreeInARow+" "+index);
+                        workingIndices.add(index);
+                    }
+                }
+            }
+            index++;
+        }
+        for (int i=0; i<FiveInARow.size(); i++) {
             if (FiveInARow.get(i).size()>0) {
                 System.out.println(FiveInARow.get(i)+" "+i);
             }
         }
-        /*
-         int index=0;
-        int count=0;
-        int[] workingIndices = new int[64];
-        while (count<64) {
-            String hash = MD5.hash("qzyelonm", index);
-            HashSet<Integer> ThreeInARow = new ArrayList<Integer>();
-            for (int i=0; i<hash.length()-2; i++) {
-                if (hash.charAt(i)==hash.charAt(i+1) && hash.charAt(i)==hash.charAt(i+2)) {
-                    ThreeInARow.add(i);
-                }
-            }
-
+        int ans=0;
+        for (Integer n : workingIndices) {
+            if (n>ans) {ans=n;}
         }
-         */
-        
+        System.out.println(ans);
+        System.out.println(workingIndices.size());
+        System.out.println(workingIndices);
     }
 }
