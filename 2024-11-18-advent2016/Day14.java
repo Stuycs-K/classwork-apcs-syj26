dd218dc7a294725ef286785b723e97bf53430b02
Sophia Jin <sjin60@stuy.edu>
2024-11-22 20:21:38 -0500

    almost solved oops

--- 2024-11-18-advent2016/Day14.java
+++ 2024-11-18-advent2016/Day14.java
@@ -3,29 +3,32 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Day14 {
-    public static void FiveInARowSet(int index, String hash, ArrayList<HashSet<String>> FiveInARow) {
+    public static void FiveInARowSet(int index, String hash, ArrayList<String> FiveInARow) {
+        boolean done=false;
         for (int i=0; i<hash.length()-4; i++) {
-            if (hash.charAt(i)==hash.charAt(i+1) && 
+            if (!done && hash.charAt(i)==hash.charAt(i+1) && 
             hash.charAt(i)==hash.charAt(i+2) && 
             hash.charAt(i)==hash.charAt(i+3) &&
             hash.charAt(i)==hash.charAt(i+4)) {
-                FiveInARow.get(index).add(hash.substring(i, i+1));
+                FiveInARow.set(index, hash.substring(i, i+1));
+                done=true;
             }
         }
     }
     public static void main(String[] args) {
         String salt="abc";
-        ArrayList<HashSet<String>> FiveInARow = new ArrayList<HashSet<String>>();
+        ArrayList<String> FiveInARow = new ArrayList<String>();
         //set up where a character appears 5 times in a row in the first 1000 hashes
         for (int index=0; index<1000; index++) {
-            FiveInARow.add(new HashSet<String>());
+            FiveInARow.add(null);
             FiveInARowSet(index, MD5.hash(salt, index), FiveInARow);
         }
+
         int index=0;
         HashSet<Integer> workingIndices = new HashSet<Integer>();
-        while (workingIndices.size()<64) {
+        while (workingIndices.size()<=64) {
             //increases the SIZE of FiveInARow by 1
-            FiveInARow.add(new HashSet<String>());
+            FiveInARow.add(null);
             //sets index (index+1000) of FiveInARow to be the desired HashSet
             FiveInARowSet(index+1000, MD5.hash(salt, index+1000), FiveInARow);
             //looking for ThreeInARow of the current hash of the given index
@@ -36,22 +39,24 @@ public class Day14 {
             for (int i=0; i<hash.length()-2; i++) {
                 if (hash.charAt(i)==hash.charAt(i+1) && hash.charAt(i)==hash.charAt(i+2) && !triple) {
                     ThreeInARow=hash.substring(i, i+1);
+                    triple=true;
                 }
             }
             //adds index to working indices if an element of ThreeInARow is in FiveInARow from index (index+1) to (index+1000)
             if (ThreeInARow != null) {
                 for (int j=index+1; j<=index+1000; j++) {
-                    if (FiveInARow.get(j).contains(ThreeInARow)) {
-                        System.out.println(ThreeInARow+" "+index);
+                    if (FiveInARow.get(j)!=null && FiveInARow.get(j).equals(ThreeInARow)) {
+                        System.out.println(ThreeInARow+" "+index+" "+MD5.hash(salt, index)+" from "+j+" "+MD5.hash(salt, j)+" ");
                         workingIndices.add(index);
                     }
                 }
             }
             index++;
         }
+        System.out.println("");
         for (int i=0; i<FiveInARow.size(); i++) {
-            if (FiveInARow.get(i).size()>0) {
-                System.out.println(FiveInARow.get(i)+" "+i);
+            if (FiveInARow.get(i) != null) {
+                System.out.println(FiveInARow.get(i)+" "+i+" "+MD5.hash(salt, i));
             }
         }
         int ans=0;
