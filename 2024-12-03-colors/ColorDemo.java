f7873fc4ce1b1c31ac642d66812a2357af03d404
Sophia Jin <sjin60@stuy.edu>
2024-12-03 11:59:18 -0500

    tested colors

---
+++ 2024-12-03-colors/ColorDemo.java
@@ -0,0 +1,8 @@
+public class ColorDemo {
+  public static void main(String[] args) {
+    System.out.print("\u001b[31mRed");
+    System.out.print(" \u001b[0mfish ");
+    System.out.print("\u001b[36mBlue");
+    System.out.print(" \u001b[0mfish ");
+  }
+}
