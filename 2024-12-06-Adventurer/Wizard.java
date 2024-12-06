74a89f0f771e3495f041a2845ce67bcebbca8d3c
Sophia Jin <sjin60@stuy.edu>
2024-12-06 11:21:39 -0500

    wrote accessor methods

--- 2024-12-06-Adventurer/Wizard.java
+++ 2024-12-06-Adventurer/Wizard.java
@@ -1,16 +1,33 @@
 public class Wizard extends Adventurer {
   private int mana;
+
+  //constructors
   public Wizard(String name) {
     super(name);
     mana=50;
   }
+
   public Wizard(String name, int hp) {
     super(name, hp);
     mana=50;
   }
+
   public Wizard(String name, int hp, int mana) {
     super(name, hp);
     this.mana=mana;
   }
-}
 
+  //methods
+  public String getSpecialName() {
+    return "mana";
+  }
+  public int getSpecial() {
+    return mana;
+  }
+  public void setSpecial(int n) {
+    mana=n;
+  }
+  public int getSpecialMax() {
+    return 100;
+  }
+}
