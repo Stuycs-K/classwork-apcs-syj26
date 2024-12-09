7191b94f13931fb534243ccda202488e1a2cf87b
Sophia Jin <sjin60@stuy.edu>
2024-12-09 14:54:05 -0500

    added more messages, edited values

--- 2024-12-06-Adventurer/Wizard.java
+++ 2024-12-06-Adventurer/Wizard.java
@@ -36,7 +36,7 @@ public class Wizard extends Adventurer {
 
   //hurt or hinder the target adventurer
   public String attack(Adventurer other) {
-    int decHP=(int)(Math.random()*4);
+    int decHP=(int)(Math.random()*4+1);
     other.applyDamage(decHP);
     String[] messages=new String[]{" smacks "+other+" with their magic staff", 
     " karate kicks "+other+" in the head", 
@@ -48,30 +48,41 @@ public class Wizard extends Adventurer {
 
   //heall or buff the target adventurer
   public String support(Adventurer other) {
-    int incHP=(int)(Math.random()*4);
-    int incSpecial=(int)(Math.random()*10);
+    int incHP=(int)(Math.random()*4+1);
+    int incSpecial=(int)(Math.random()*5+5);
     other.setHP(other.getHP()+incHP);
     other.restoreSpecial(incSpecial);
-    return getName()+" increased "+other.getName()+"'s HP by "+incHP+", increased "+other.getName()+"'s "+getSpecialName()+" by "+incSpecial+".";
+    String[] messages=new String[]{" gives "+other+" a healing potion",
+    " casts a restorative spell on "+other};
+    int index=(int)(Math.random()*messages.length);
+    return this+messages[index]+" increasing "+other+"'s HP by "+incHP+" and "+other+"'s "+getSpecialName()+" by "+incSpecial+".";
   }
 
   //heall or buff self
   public String support() {
     int incHP=(int)(Math.random()*(getmaxHP()/2));
-    int incSpecial=(int)(Math.random()*10);
+    int incSpecial=(int)(Math.random()*5+5);
     setHP(getHP()+incHP);
     restoreSpecial(incSpecial);
-    return "increased own HP by "+incHP+", increased own "+getSpecialName()+" by "+incSpecial+".";
+    String[] messages=new String[]{" drinks a healing potion",
+    " casts a restorative spell on themself",
+    " gets over 8 hours of sleep"};
+    int index=(int)(Math.random()*messages.length);
+    return this+messages[index]+", increasing their HP by "+incHP+" and their "+getSpecialName()+" by "+incSpecial+".";
   }
 
   //hurt or hinder the target adventurer, consume some special resource
   public String specialAttack(Adventurer other) {
     int cost=(int)(Math.random()*(getSpecialMax()/10));
     if (cost<=getSpecial()) {
-      int decHP=(int)(Math.random()*10);
+      int decHP=(int)(Math.random()*5+5);
       other.applyDamage(decHP);
       setSpecial(getSpecial()-cost);
-      return getName()+" decreased "+other.getName()+"'s HP by "+decHP+", consuming "+cost+" "+getSpecialName();
+      String[] messages=new String[]{" casts a spell on "+other,
+      " throws an explosive potion at "+other,
+      " spreads sparkly magical glitter in the air"};
+      int index=(int)(Math.random()*messages.length);
+      return this+messages[index]+", decreasing "+other+"'s HP by "+decHP+", consuming "+cost+" "+getSpecialName();
     } else {
       return "Not enough mana to attack. Instead, "+attack(other);
     }
