343600117e1a879ba244dc131f5d00ca3d66c557
Sophia Jin <sjin60@stuy.edu>
2025-01-03 11:46:08 -0500

    wrote toWords() up to 99

--- 2025-01-02/Words.java
+++ 2025-01-02/Words.java
@@ -8,6 +8,23 @@ public class Words {
       }
     }
   }
+
+  public static String toWords(int n) {
+    String[] teens = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+    "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+    String[] ten1 = {"twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+    if (n<20) {
+      return teens[n];
+    } else if (n<100) {
+      if (n%10 != 0) {
+        return ten1[n/10-2]+"-"+teens[n%10];
+      } else {
+        return ten1[n/10-2];
+      }
+    } else {
+      return "too big";
+    }
+  }
   public static void main(String[] args) {
     makeWords(2, "", "abc");
     System.out.println("");
@@ -20,5 +37,8 @@ public class Words {
     makeWords(1, "", "abcdefghijklmnopqrstuvwxyz");
     System.out.println("");
     makeWords(0, "", "abcdefghijklmnopqrstuvwxyz");
+    for (int i=0; i<100; i++) {
+      System.out.println(toWords(i));
+    }
   }
 }
